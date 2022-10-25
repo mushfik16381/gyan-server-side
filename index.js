761d907e7ee088721dc2921eedcfdebfec709d17
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-10-26 02:09:21 +0600

    server product added

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const port = process.env.port || 5000;
+
+const productCollection = require("./Data/product.json")
+
+
+app.get("/products", (req, res) => {
+    res.send(productCollection);
+});
+
+app.get("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const getSingleItem = productCollection?.find((p) => p.id == id);
+    if(!getSingleItem){
+        res.send("Product Not Found");
+    }
+    res.send(getSingleItem);
+});
+
+app.listen(port, () => {
+    console.log('server is running', port)
+})
